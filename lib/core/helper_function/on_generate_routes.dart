ff28b35225f837058a773baf2a271e9c789240ba
peter8879 <petergeorge65154@gmail.com>
2025-04-02 15:27:50 +0200

    Init project files

---
+++ lib/core/helper_function/on_generate_routes.dart
@@ -0,0 +1,11 @@
+//generate Routes
+import 'package:flutter/material.dart';
+import 'package:coins_hub/features/home/presentation/views/home_view.dart';
+Route <dynamic> onGenerateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case HomeView.routeName:
+      return MaterialPageRoute(builder: (_) => const HomeView());
+    default:
+      return MaterialPageRoute(builder: (_) => const HomeView());
+  }
+}
---
+++ lib/features/home/presentation/views/home_view.dart
@@ -0,0 +1,14 @@
+import 'package:coins_hub/features/home/presentation/views/widgets/home_view_body.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+  static const String routeName='Home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HomeViewBody(),
+    );
+  }
+}
